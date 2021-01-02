0a51fb57c6b18a7faa85826eabe6c4a360e31d3b
Yerdaulet <47812867+syzygy17@users.noreply.github.com>
2021-01-02 22:43:02 +0600

    Stage 4/6
    Completed the fourth stage

--- browser.py
+++ browser.py
@@ -1,86 +1,37 @@
 import os
 import sys
+import requests
 
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
 
 dir_name = sys.argv[1]
 os.makedirs(dir_name, exist_ok=True)
 tabs_list = []
 stack_back = []
 
-BLOOMBERG = 'bloomberg'
-NYTIMES = 'nytimes'
-BLOOMBERG_SITE = 'bloomberg.com'
-NYTIMES_SITE = 'nytimes.com'
 EXIT = 'exit'
 TB_TABS = 'tb_tabs/'
 BACK = 'back'
+HTTPS = 'https://'
 
 while True:
     url = input()
-    if url != BLOOMBERG_SITE and url != NYTIMES_SITE:
-        if url == EXIT:
-            break
-        if url in tabs_list:
-            if url == BLOOMBERG:
-                print(bloomberg_com)
-            elif url == NYTIMES:
-                print(nytimes_com)
-        else:
-            if url == BACK and len(stack_back) > 0:
-                stack_back.pop()
-                if stack_back[-1] == BLOOMBERG:
-                    print(bloomberg_com)
-                elif stack_back[-1] == NYTIMES:
-                    print(nytimes_com)
-            else:
-                print('Error: Incorrect URL')
+    if url == EXIT:
+        break
+    elif '.' not in url:
+        print('Error: Incorrect URL')
     else:
-        if url == BLOOMBERG_SITE and url not in tabs_list:
-            print(bloomberg_com)
-            f = open(TB_TABS + BLOOMBERG, 'w')
-            f.write(bloomberg_com)
+        r = requests.get(HTTPS + url)
+        if url in tabs_list:
+            print(r.text)
+        elif url == BACK and len(stack_back) > 0:
+            stack_back.pop()
+            print(stack_back[-1])
+        elif url not in tabs_list:
+            print(r.text)
+            f = open(TB_TABS + url, 'w', encoding='UTF-8')
+            f.write(r.text)
             f.close()
-            tabs_list.append(BLOOMBERG)
-            stack_back.append(BLOOMBERG)
-        elif url == NYTIMES_SITE and url not in tabs_list:
-            print(nytimes_com)
-            f = open(TB_TABS + NYTIMES, 'w')
-            f.write(nytimes_com)
-            f.close()
-            tabs_list.append(NYTIMES)
-            stack_back.append(NYTIMES)
+            tabs_list.append(url)
+            stack_back.append(r.text)
+
 
